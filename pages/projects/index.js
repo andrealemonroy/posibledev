ab9dfbd3d4b7ba84e91323368eea40889ae3dfe3
andrealemonroy <andreale17@icloud.com>
2021-05-22 11:16:56 -0500

    add layout in projects

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,8 +2,9 @@ import Layout from "../../components/Layout";
 export default function Projects() {
   return (
     <>
-      <Layout></Layout>
-      <h1>Proyectos</h1>
+      <Layout>
+        <h1>Proyectos</h1>
+      </Layout>
     </>
   );
 }
